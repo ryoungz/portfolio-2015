a172252b03d41af1a95778ac06586faa2d4106bc
Robert Young <ryoungz@gmail.com>
2015-09-05 16:02:27 -0500

    initial commit. gulpfile setup for portfolio.

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('hello', function() {
+  console.log('Hello Robert');
+});
+
+//Sass task
+gulp.task('styles', function() {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError)) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('./app/css/'))
+    .pipe(browserSync.reload({
+       stream: true
+    }))
+});
+
+//Watch task
+gulp.task('watch', ['browserSync', 'styles'], function() {
+  gulp.watch('app/scss/*.scss',['styles']);
+});
+
+//Browser sync
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+
